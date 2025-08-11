e06b107c8b59203fefdb3a2d8bdcb8a40c8f009a
Allyne77 <allyneninie@gmail.com>
2025-08-11 12:39:23 +0800

    wassupp

---
+++ lab06/exercise3.py
@@ -0,0 +1,9 @@
+full_name = "ALI"
+print ("Enter your name: ", full_name)
+uppercase_name = full_name.upper()
+print("Uppercase name: ", uppercase_name)
+lowercase_name = full_name.lower()
+print("Lowercase name: ", lowercase_name)
+
+print("Name length: ", len(full_name))
+print(full_name)
---
+++ lab06/exercise4.py
@@ -0,0 +1,9 @@
+# Without \n - everything prints on one line
+print("============RECEIPT=============")
+
+# With \n - creates line breaks
+print("Item\nPrice\nQty\nTotal")
+print("Coffee\n$3.50\n2\n$7.00")
+
+print("=========================")
+
