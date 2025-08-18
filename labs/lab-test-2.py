d808e90e798e4d60f504c566f87e409ddf0eca46
Allyne77 <allyneninie@gmail.com>
2025-08-18 11:19:28 +0800

    hey

---
+++ labs/lab-test-2.py
@@ -0,0 +1,28 @@
+Name = "Ahmad Bin Abu"
+MatricNo = "MS2025123499"
+message1 = "This is my"
+message2 = "        second"
+message3 = "             assignment"
+message4 = "I want 2x10 marks,which is 20 full marks"
+
+
+print("Name :" +Name +                 "\t"  +              "                          Matric No :" + MatricNo)
+
+
+
+
+print("*                       *")
+print("**               **")
+print("***    ***")
+print("********")
+print("***    ***")
+print("**               **")
+print("*                       *")
+
+
+
+print("", message1)
+print("", message2)
+print("", message3)
+print("", message4)
+
