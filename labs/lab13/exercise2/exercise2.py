048877accea415e7753d2817a1f998a6166e21ba
Allyne77 <allyneninie@gmail.com>
2025-10-27 13:08:57 +0800

    lb13

--- labs/lab13/exercise2/exercise2.py
+++ labs/lab13/exercise2/exercise2.py
@@ -6,7 +6,8 @@ found_number = False
 for number in range(20):
     print(f'Checking number: {number}')
     if number == target1 or number == target2:
-        found_number = True
-        break
+       found_number = True
+       print(f'Found target number: {number}')
+       continue
 
 print(found_number)
--- labs/lab13/exercise3/exercise3.py
+++ labs/lab13/exercise3/exercise3.py
@@ -1,5 +1,6 @@
 grade = float(input())
-
+valid_count = 0
+total = 0.0
 # TODO: Your code here
 
 print(valid_count)
